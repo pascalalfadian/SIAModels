8456cc6d5ad499ad5e292803ab3cdfe47153aa8d
MuhammadTaufikA <muhammad.taufikadianto@yahoo.com>
2016-02-26 13:47:01 +0700

    mengubah AIF405 - AIF 462

--- src/main/java/id/ac/unpar/siamodels/matakuliah/AIF405.java
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF405.java
@@ -6,26 +6,26 @@ import id.ac.unpar.siamodels.matakuliah.interfaces.Projek;
 
 import java.util.List;
 
+/**
+ * Mata kuliah ini merupakan lanjutan dari Projek Sistem Informasi 1 dan
+ * memberikan kesempatan bagi mahasiswa untuk melanjutkan/mengembangkan
+ * perancangan sitem pada organisasi studi kasus, mengimplementasikan rancangan
+ * dan melakukan pengujian perangkat lunak;
+ *
+ * @author Veronica S. Moertini (moertini@unpar.ac.id)
+ */
+
 @MataKuliah(kode = "AIF405", nama = "Proyek Sistem Informasi 2", sks = 3)
 public class AIF405 implements HasPrasyarat, Projek, HasPraktikum {
 
-	@Override
-	public boolean checkPrasyarat(Mahasiswa mahasiswa, List<String> reasonsContainer) {
-		boolean ok = true;
-		if (!mahasiswa.hasTempuhKuliah("AIF304")) {
-			reasonsContainer.add("Tidak memenuhi prasyarat tempuh AIF304");			
-			ok = false;
-		}
-		return ok;
-	}
-        
-        /**
-         * @author Veronica S. Moertini (moertini@unpar.ac.id)
-         * @return deskripsi mata kuliah
-         */
-        public String getDeskripsi(){
-            return "Mata kuliah ini merupakan lanjutan dari Projek Sistem Informasi 1 dan memberikan "
-                    + "kesempatan bagi mahasiswa untuk melanjutkan/mengembangkan perancangan sitem pada organisasi studi kasus, "
-                    + "mengimplementasikan rancangan dan melakukan pengujian  perangkat lunak;";
+    @Override
+    public boolean checkPrasyarat(Mahasiswa mahasiswa, List<String> reasonsContainer) {
+        boolean ok = true;
+        if (!mahasiswa.hasTempuhKuliah("AIF304")) {
+            reasonsContainer.add("Tidak memenuhi prasyarat tempuh AIF304");
+            ok = false;
         }
+        return ok;
+    }
+
 }
