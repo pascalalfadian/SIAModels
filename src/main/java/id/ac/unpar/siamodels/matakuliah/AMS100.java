65285c1182c67c44e67490c0c7686ad4543b00d0
Rachael Awuy <rachaelawuy@hotmail.com>
2016-02-26 13:24:22 +0700

    Mengubah author untuk AIF 469 dan AMS 100

--- src/main/java/id/ac/unpar/siamodels/matakuliah/AMS100.java
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AMS100.java
@@ -2,15 +2,13 @@ package id.ac.unpar.siamodels.matakuliah;
 
 import id.ac.unpar.siamodels.matakuliah.interfaces.Wajib;
 
+/**
+ * Sistem Bilangan, Fungsi, Limit dan Kekontinuan Fungsi, Turunan, Integral, 
+ * Penggunaan Integral, Sistem Persamaan Linear, Determinan, Vektor, Nilai dan 
+ * Vektor Eigen.
+ * @author Taufik Limansyah, S.Si, MT.
+ */
 @MataKuliah(kode = "AMS100", nama = "Matematika Informatika", sks = 4)
 public class AMS100 implements Wajib {
     
-    /**
-     * @author Taufik Limansyah, S.Si, MT.
-     * @return 
-     */
-    public String getDeskripsi(){
-        return "Sistem Bilangan, Fungsi, Limit dan Kekontinuan Fungsi, Turunan, Integral, Penggunaan Integral, "
-                + "Sistem Persamaan Linear, Determinan, Vektor, Nilai dan Vektor Eigen.";
-    }
 }
