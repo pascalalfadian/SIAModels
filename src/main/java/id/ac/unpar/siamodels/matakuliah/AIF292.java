270f287e81e660a83f440e95cd6323a2ac95ac1e
NADIA ANDINI KAMIL <FTIS\i12050@lab01-16u.ftis.unpar>
2016-04-28 13:22:44 +0700

    diadd ya

---
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF292.java
@@ -0,0 +1,10 @@
+package id.ac.unpar.siamodels.matakuliah;
+
+import id.ac.unpar.siamodels.InfoMataKuliah;
+import id.ac.unpar.siamodels.MataKuliah;
+
+
+@InfoMataKuliah(nama = "Desain & Analisis Algoritma", sks = 3)
+public class AIF292 extends MataKuliah {
+
+}
---
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF352.java
@@ -0,0 +1,10 @@
+package id.ac.unpar.siamodels.matakuliah;
+
+import id.ac.unpar.siamodels.InfoMataKuliah;
+import id.ac.unpar.siamodels.MataKuliah;
+
+
+@InfoMataKuliah(nama = "Jaringan Syaraf Tiruan", sks = 2)
+public class AIF352 extends MataKuliah {
+
+}
