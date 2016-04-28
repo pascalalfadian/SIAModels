d8a5cfd7cdc1e88243e5a824aac7814fe07d1820
MICHAEL ADRIAN <FTIS\i13039@lab01-15u.ftis.unpar>
2016-04-28 12:31:22 +0700

    Edit AIF352, add AIF347, AIF450, AIF460

--- src/main/java/id/ac/unpar/siamodels/matakuliah/AIF347.java
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF347.java
@@ -3,7 +3,7 @@ package id.ac.unpar.siamodels.matakuliah;
 import id.ac.unpar.siamodels.MataKuliah;
 import id.ac.unpar.siamodels.InfoMataKuliah;
 
-@InfoMataKuliah(nama = "Jaringan Syaraf Tiruan", sks = 2)
-public class AIF352 extends MataKuliah {
+@InfoMataKuliah(nama = "E-Commerce", sks = 2)
+public class AIF347 extends MataKuliah {
 
 }
