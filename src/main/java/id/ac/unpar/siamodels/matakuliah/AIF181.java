9d2b8de5ab524a3d598d75e500ac4d5c370e5999
MICHAEL ADRIAN <FTIS\i13039@lab01-15u.ftis.unpar>
2016-04-29 14:48:51 +0700

    Remove Deprecated tag

--- src/main/java/id/ac/unpar/siamodels/matakuliah/AIF181.java
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF181.java
@@ -3,9 +3,6 @@ package id.ac.unpar.siamodels.matakuliah;
 import id.ac.unpar.siamodels.InfoMataKuliah;
 import id.ac.unpar.siamodels.MataKuliah;
 
-/**
- * @deprecated Mata kuliah tidak dibuka lagi.
- */
 @InfoMataKuliah(nama = "Dasar-dasar Pemrograman", sks = 3)
 public class AIF181 extends MataKuliah {
 
