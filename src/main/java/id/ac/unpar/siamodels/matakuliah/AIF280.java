16f7b922f8f9f3ab034dd848a68230aa4c14f862
MICHAEL ADRIAN <FTIS\i13039@lab01-15u.ftis.unpar>
2016-04-29 14:52:42 +0700

    Remove Deprecated tag

--- src/main/java/id/ac/unpar/siamodels/matakuliah/AIF280.java
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF280.java
@@ -3,9 +3,6 @@ package id.ac.unpar.siamodels.matakuliah;
 import id.ac.unpar.siamodels.InfoMataKuliah;
 import id.ac.unpar.siamodels.MataKuliah;
 
-/**
- * @deprecated Mata kuliah tidak dibuka lagi.
- */
 @InfoMataKuliah(nama = "Praktika Interaksi Manusia Komputer", sks = 1)
 public class AIF280 extends MataKuliah {
 
