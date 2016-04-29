fb82e9431c01830b41164a3bfc3ab0efbc4ec6e5
Nadia Andini <nadiaura1995@gmail.com>
2016-04-30 05:50:19 +0700

    Menambahkan mata kuliah yang belum terdaftar
    * Edit AIF341, AIF342, AIF441, AIF442, AIF352
    
    * Edit AIF352, add AIF347, AIF450, AIF460
    
    * kerjaan Reinhart
    
    * diadd ya
    
    * tambah
    
    * sudah
    
    * Add Deprecated tag
    
    * Remove Deprecated tag
    
    * Remove Deprecated tag
    
    * Add Deprecated tag
    
    * add APS302.java
    
    * kelas ambigu: APS302 dan APS309

--- src/main/java/id/ac/unpar/siamodels/matakuliah/AIF441.java
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF441.java
@@ -1,8 +1,12 @@
 package id.ac.unpar.siamodels.matakuliah;
 
+import java.util.List;
+
 import id.ac.unpar.siamodels.InfoMataKuliah;
+import id.ac.unpar.siamodels.Mahasiswa;
 import id.ac.unpar.siamodels.MataKuliah;
 import id.ac.unpar.siamodels.matakuliah.interfaces.HasPraktikum;
+import id.ac.unpar.siamodels.matakuliah.interfaces.HasPrasyarat;
 
 /**
  * Mata kuliah ini memperkenalkan kepada mahasiswa konsep jaringan lanjut
@@ -14,6 +18,15 @@ import id.ac.unpar.siamodels.matakuliah.interfaces.HasPraktikum;
  */
 
 @InfoMataKuliah(nama = "Administrasi Jaringan Komputer 3", sks = 3)
-public class AIF441 extends MataKuliah implements HasPraktikum {
+public class AIF441 extends MataKuliah implements HasPraktikum, HasPrasyarat {
 
+	@Override
+	public boolean checkPrasyarat(Mahasiswa mahasiswa, List<String> reasonsContainer) {
+		if (!mahasiswa.hasLulusKuliah("AIF342")) {
+			reasonsContainer.add("Tidak memenuhi prasyarat lulus AIF342");			
+			return false;
+		}
+		return true;
+	}
+	
 }
