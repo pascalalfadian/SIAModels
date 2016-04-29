f1cdc87f72b8e290da159b1e7e02d3ea82817b2c
MICHAEL ADRIAN <FTIS\i13039@lab01-15u.ftis.unpar>
2016-04-29 14:43:47 +0700

    Add Deprecated tag

--- src/main/java/id/ac/unpar/siamodels/matakuliah/AIF182.java
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF182.java
@@ -3,7 +3,9 @@ package id.ac.unpar.siamodels.matakuliah;
 import id.ac.unpar.siamodels.InfoMataKuliah;
 import id.ac.unpar.siamodels.MataKuliah;
 
-
+/**
+ * @deprecated Mata kuliah tidak dibuka lagi.
+ */
 @InfoMataKuliah(nama = "Pengantar Basis Data", sks = 2)
 public class AIF182 extends MataKuliah {
 
--- src/main/java/id/ac/unpar/siamodels/matakuliah/AIF183.java
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/AIF183.java
@@ -3,6 +3,9 @@ package id.ac.unpar.siamodels.matakuliah;
 import id.ac.unpar.siamodels.InfoMataKuliah;
 import id.ac.unpar.siamodels.MataKuliah;
 
+/**
+ * @deprecated Mata kuliah tidak dibuka lagi.
+ */
 @InfoMataKuliah(nama = "Pemrograman Prosedural", sks = 4)
 public class AIF183 extends MataKuliah {
 
