93e544dd75503b14d249b85d2a11c7f394732f88
Andrianto Sugiarto <andriantosugiarto@gmail.com>
2018-09-18 04:58:45 +0700

    update some mata kuliah kurikulum 2018 not everything done

---
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/kurikulum2018/AIF182112.java
@@ -0,0 +1,23 @@
+package id.ac.unpar.siamodels.matakuliah.kurikulum2018;
+
+import id.ac.unpar.siamodels.Mahasiswa;
+import id.ac.unpar.siamodels.InfoMataKuliah;
+import id.ac.unpar.siamodels.MataKuliah;
+import id.ac.unpar.siamodels.matakuliah.interfaces.*;
+
+import java.util.List;
+
+
+@InfoMataKuliah(nama = "Pemrograman Kompetitif 2", sks = 3)
+public class AIF18212 extends MataKuliah implements HasPraktikum, HasPrasyarat{
+
+    @Override
+    public boolean checkPrasyarat(Mahasiswa mahasiswa, List<String> reasonsContainer) {
+        boolean ok = true;
+        if (!mahasiswa.hasLulusKuliah("AIF182101")) {
+            reasonsContainer.add("Tidak memenuhi prasyarat lulus AIF182101");
+            ok = false;
+        }
+        return ok;
+    }
+}
---
+++ src/main/java/id/ac/unpar/siamodels/matakuliah/kurikulum2018/AIF183120.java
@@ -0,0 +1,23 @@
+package id.ac.unpar.siamodels.matakuliah.kurikulum2018;
+
+import id.ac.unpar.siamodels.InfoMataKuliah;
+import id.ac.unpar.siamodels.MataKuliah;
+import id.ac.unpar.siamodels.matakuliah.interfaces.HasPrasyarat;
+import id.ac.unpar.siamodels.Mahasiswa;
+
+import java.util.List;
+
+
+@InfoMataKuliah(nama = "Perancangan Permainan Komputer", sks = 3)
+public class AIF183120 extends MataKuliah implements HasPrasyarat{
+
+    @Override
+    public boolean checkPrasyarat(Mahasiswa mahasiswa, List<String> reasonsContainer) {
+        boolean ok = true;
+        if (!mahasiswa.hasTempuhKuliah("AIF183117")) {
+            reasonsContainer.add("Tidak memenuhi prasyarat tempuh AIF183117");
+            ok = false;
+        }
+        return ok;
+    }
+}
